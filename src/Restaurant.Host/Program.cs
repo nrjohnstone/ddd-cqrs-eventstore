8ba3d8c4d3e0526d15f94802e68f4f783f05b097
nrjohnstone <locotewz@gmail.com>
2016-10-26 06:43:09 +0100

    restructure program layout to aid in understanding

--- src/Restaurant.Host/Program.cs
+++ src/Restaurant.Host/Program.cs
@@ -13,10 +13,17 @@ namespace Restaurant.Host
     {
         private static Random _random;
         private static List<IStartable> _startables;
-        private static TimeToLiveHandler<OrderPlaced> _cookBob;
-        private static TimeToLiveHandler<OrderPlaced> _cookPaco;
-        private static TimeToLiveHandler<OrderPlaced> _cookSteve;
+        private static TimeToLiveHandler<OrderPlaced> _ttlBob;
+        private static TimeToLiveHandler<OrderPlaced> _ttlPaco;
+        private static TimeToLiveHandler<OrderPlaced> _ttlSteve;
         private static TopicPublisher _publisher;
+        private static PrinterHandler _printerHandler;
+        private static Waiter _waiter;
+        private static Cashier _cashier;
+        private static AssistantManager _asstManager;
+        private static Cook _cookBob;
+        private static Cook _cookPaco;
+        private static Cook _cookSteve;
 
         static void Main(string[] args)
         {
@@ -24,36 +31,82 @@ namespace Restaurant.Host
             _startables = new List<IStartable>();
             _publisher = new TopicPublisher();
 
-            var printerHandler = new PrinterHandler();
+            #region "Create Actors"
 
-            var cashier = new Cashier(_publisher);
+            _printerHandler = new PrinterHandler();
+            _waiter = new Waiter(_publisher);
+            _cashier = new Cashier(_publisher);
+            _asstManager = new AssistantManager(_publisher);
 
-            var cashierQueue = CreateQueueThreadHandler(cashier);
+            _cookBob = new Cook("Bob", 10, _publisher);
+            _cookPaco = new Cook("Paco", 25, _publisher);
+            _cookSteve = new Cook("Steve", 5000, _publisher);
 
-            var asstManager = new AssistantManager(_publisher);
+            #endregion
 
-            var cookHandlers = CreateCooks();
+            #region "Create TTL Handlers"
 
-            var dispatcher = new BalancedRoundRobin<OrderPlaced>(cookHandlers);
-            var cooksDispatcherQueue = new QueueThreadedHandler<OrderPlaced>(dispatcher);
-            
-            _startables.Add(cooksDispatcherQueue);
+            _ttlBob = new TimeToLiveHandler<OrderPlaced>(_cookBob);
+            _ttlPaco = new TimeToLiveHandler<OrderPlaced>(_cookPaco);
+            _ttlSteve = new TimeToLiveHandler<OrderPlaced>(_cookSteve);
 
-            var waiter = new Waiter(_publisher);
+            #endregion
+
+            #region "Create Queues"
+
+            var cashierQueue = new QueueThreadedHandler<OrderPriced>(_cashier);
+            var cookBobQueue = new QueueThreadedHandler<OrderPlaced>(_ttlBob);
+            var cookPacoQueue = new QueueThreadedHandler<OrderPlaced>(_ttlPaco);
+            var cookSteveQueue = new QueueThreadedHandler<OrderPlaced>(_ttlSteve);
+
+            var cooksReceieveQueue = new BalancedRoundRobin<OrderPlaced>(new[]
+            {
+                cookBobQueue,
+                cookPacoQueue,
+                cookSteveQueue
+            });
+
+            var kitchenReceieveQueue = new QueueThreadedHandler<OrderPlaced>(cooksReceieveQueue);
+
+            #endregion
+
+            #region "Queue Monitoring"
+
+            var cookQueues = new[]
+            {
+                cookBobQueue,
+                cookPacoQueue,
+                cookSteveQueue
+            };
 
             var queuesToMonitor = new List<IMonitorableQueue>();
-            queuesToMonitor.AddRange(cookHandlers);
-            queuesToMonitor.Add(cooksDispatcherQueue);
+            queuesToMonitor.AddRange(cookQueues);
+            queuesToMonitor.Add(kitchenReceieveQueue);
             var queueMonitor = new QueueMonitor(queuesToMonitor);
+
+            #endregion
+
+            #region "Subscribe to Events"
+
+            _publisher.Subscribe(kitchenReceieveQueue);
+            _publisher.Subscribe(_asstManager);
+            _publisher.Subscribe(cashierQueue);
+            _publisher.Subscribe(_printerHandler);
+
+            #endregion
+              
+            #region "Start threads"
+
+            _startables.Add(cashierQueue);
+            _startables.Add(cookBobQueue);
+            _startables.Add(cookPacoQueue);
+            _startables.Add(cookSteveQueue);
+            _startables.Add(kitchenReceieveQueue);
             _startables.Add(queueMonitor);
 
-            // Subscriptions
-            _publisher.Subscribe(cooksDispatcherQueue);
-            _publisher.Subscribe(asstManager);
-            _publisher.Subscribe(cashierQueue);
-            _publisher.Subscribe(printerHandler);
-
             _startables.ForEach(x => x.Start());
+
+            #endregion
 
             bool shutdown = false;
             int ordersCreated = 0;
@@ -62,66 +115,36 @@ namespace Restaurant.Host
             {
                 if (ordersCreated < 100)
                 {
-                    PlaceOrders(waiter, 5);
+                    PlaceOrders(_waiter, 5);
                     ordersCreated += 5;
                 }
                 
-                var outstandingOrders = cashier.GetOutstandingOrders();
+                var outstandingOrders = _cashier.GetOutstandingOrders();
 
                 PrintOutstandingOrders(outstandingOrders);
 
                 foreach (var restaurantDocument in outstandingOrders)
                 {
-                    cashier.Pay(restaurantDocument.Id);
+                    _cashier.Pay(restaurantDocument.Id);
                 }
                 
-                var outstandingOrdersAfterPay = cashier.GetOutstandingOrders();
+                var outstandingOrdersAfterPay = _cashier.GetOutstandingOrders();
                 PrintOutstandingOrders(outstandingOrdersAfterPay);
                 PrintDroppedMessages();
-                Thread.Sleep(100);
+                Thread.Sleep(500);
             }
         }
 
         private static void PrintDroppedMessages()
         {
             var totalOrdersDropped = 
-                _cookBob.TotalOrdersDropped +
-                _cookPaco.TotalOrdersDropped +
-                _cookSteve.TotalOrdersDropped;
+                _ttlBob.TotalOrdersDropped +
+                _ttlPaco.TotalOrdersDropped +
+                _ttlSteve.TotalOrdersDropped;
             Console.WriteLine($"Total dropped messages {totalOrdersDropped}");
-            Console.WriteLine($"Bob dropped messages {_cookBob.TotalOrdersDropped}");
-            Console.WriteLine($"Paco dropped messages {_cookPaco.TotalOrdersDropped}");
-            Console.WriteLine($"Steve dropped messages {_cookSteve.TotalOrdersDropped}");
-        }
-
-        private static QueueThreadedHandler<OrderPlaced>[] CreateCooks()
-        {
-            _cookBob = new TimeToLiveHandler<OrderPlaced>(
-                new Cook("Bob",
-                10, _publisher));
-
-            _cookPaco = new TimeToLiveHandler<OrderPlaced>(
-                new Cook("Paco",
-                499, _publisher));
-
-            _cookSteve = new TimeToLiveHandler<OrderPlaced>(
-                new Cook("Steve",
-                1500, _publisher));
-
-            var queueThreadHandlers = new[]
-            {
-                CreateQueueThreadHandler(_cookBob),
-                CreateQueueThreadHandler(_cookPaco),
-                CreateQueueThreadHandler(_cookSteve)
-            };
-            return queueThreadHandlers;
-        }
-
-        private static QueueThreadedHandler<T> CreateQueueThreadHandler<T>(IOrderHandler<T> cashier) where T : MessageBase
-        {
-            var cashierThread = new QueueThreadedHandler<T>(cashier);
-            _startables.Add(cashierThread);
-            return cashierThread;
+            Console.WriteLine($"Bob dropped messages {_ttlBob.TotalOrdersDropped}");
+            Console.WriteLine($"Paco dropped messages {_ttlPaco.TotalOrdersDropped}");
+            Console.WriteLine($"Steve dropped messages {_ttlSteve.TotalOrdersDropped}");
         }
 
         private static void PlaceOrders(Waiter waiter, int numberOfOrders)
